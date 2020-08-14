5219135717d0b2c2a5e7bb080360be862b76d675
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-08-14 21:09:10 +0100

    Implimenting redux global store

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import Posts from './components/Posts';
 import Postform from './components/Postform';
+import { Provider } from 'react-redux';
+import store from './store';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Dummy Blog</h1>
-        <Postform />
-        <br />
-        <hr />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <h1>Dummy Blog</h1>
+          <Postform />
+          <br />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
-
-export default App;
