c43750b499d96c98e9215c9f610b23917be6a7b6
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-08-14 04:00:43 +0100

    form input state, onChange

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -6,7 +6,13 @@ export default class Postform extends Component {
     body: "",
   };
 
+  onChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
   render() {
+    const { title, body } = this.state;
     return (
       <div>
         <h2>Add post</h2>
@@ -14,12 +20,23 @@ export default class Postform extends Component {
           <div>
             <label>Title:</label>
             <br />
-            <input type="text" name="title" />
+            <input
+              type="text"
+              name="title"
+              value={title}
+              onChange={this.onChange}
+            />
           </div>
           <div>
             <label>Body:</label>
             <br />
-            <textarea name="body" cols="30" rows="3" />
+            <textarea
+              name="body"
+              cols="30"
+              rows="3"
+              value={body}
+              onChange={this.onChange}
+            />
           </div>
           <br />
           <button type="submit">Submit</button>
